57ee8b62189b3fbcce8a9180ce957d17d18153d8
irfanobstinate <63938001+irfanobstinate@users.noreply.github.com>
2025-12-25 22:22:13 +0500

    newUpdate2

--- routes/product.js
+++ routes/product.js
@@ -4,5 +4,8 @@ const productController = require('../controllers/productController');
 
 router.post('/', productController.createProduct);
 router.get('/', productController.getProducts);
+router.get('/:id', productController.getProductById);
+router.put('/:id', productController.updateProduct);
+router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
